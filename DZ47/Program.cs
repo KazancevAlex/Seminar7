d5895a6da9fa33b7d055f9ba387f90d63dd569ab
Alexandr <kazancevalex1987@gmail.com>
2022-10-27 10:31:17 +1200

    Домашнее задание 47

---
+++ DZ47/Program.cs
@@ -0,0 +1,22 @@
+﻿// Домашнее задание 47
+
+void InputMatrix(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-10, 10)) ;
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество строк  ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[m, n];
+InputMatrix(numbers);
